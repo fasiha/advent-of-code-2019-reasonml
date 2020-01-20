6398a9f25e0288a651efd7b3d5104563d27bd63e
Ahmed Fasih <wuzzyview@gmail.com>
2020-01-19 22:38:05 -0500

    scala 2a

--- src/main/scala/aoc.scala
+++ src/main/scala/aoc.scala
@@ -32,4 +32,53 @@ object Main extends App {
     assert(answer1b == 5077155)
   }
 
+  def intcode(input: Array[Int]) = {
+    val program = input.clone()
+    var position = 0
+    while (program(position) != 99) {
+      program(position) match {
+        case 1 =>
+          program(program(position + 3)) = program(program(position + 1)) + program(
+            program(position + 2)
+          )
+        case 2 =>
+          program(program(position + 3)) = program(program(position + 1)) * program(
+            program(position + 2)
+          )
+        case _ => ()
+      }
+      position += 4
+    }
+    program
+  }
+
+  {
+    assert(intcode(Array(1, 0, 0, 0, 99)).sameElements(Array(2, 0, 0, 0, 99)))
+    assert(intcode(Array(2, 3, 0, 3, 99)).sameElements(Array(2, 3, 0, 6, 99)))
+    assert(
+      intcode(Array(2, 4, 4, 5, 99, 0))
+        .sameElements(Array(2, 4, 4, 5, 99, 9801))
+    )
+    assert(
+      intcode(Array(1, 1, 1, 4, 99, 5, 6, 0, 99))
+        .sameElements(Array(30, 1, 1, 4, 2, 5, 6, 0, 99))
+    )
+  }
+
+  {
+    val input2 = Array(1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 6, 1,
+      19, 2, 19, 9, 23, 1, 23, 5, 27, 2, 6, 27, 31, 1, 31, 5, 35, 1, 35, 5, 39,
+      2, 39, 6, 43, 2, 43, 10, 47, 1, 47, 6, 51, 1, 51, 6, 55, 2, 55, 6, 59, 1,
+      10, 59, 63, 1, 5, 63, 67, 2, 10, 67, 71, 1, 6, 71, 75, 1, 5, 75, 79, 1,
+      10, 79, 83, 2, 83, 10, 87, 1, 87, 9, 91, 1, 91, 10, 95, 2, 6, 95, 99, 1,
+      5, 99, 103, 1, 103, 13, 107, 1, 107, 10, 111, 2, 9, 111, 115, 1, 115, 6,
+      119, 2, 13, 119, 123, 1, 123, 6, 127, 1, 5, 127, 131, 2, 6, 131, 135, 2,
+      6, 135, 139, 1, 139, 5, 143, 1, 143, 10, 147, 1, 147, 2, 151, 1, 151, 13,
+      0, 99, 2, 0, 14, 0)
+    input2(1) = 12
+    input2(2) = 2
+    val answer2a = intcode(input2)(0)
+    assert(answer2a == 4576384)
+  }
+
 }
